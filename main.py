50bcfb2dd8f5715994ae12cc437404138b943855
KhaiHoang <hoangkhai178@gmail.com>
2023-11-04 19:04:31 +0700

    color

--- main.py
+++ main.py
@@ -3,22 +3,18 @@ from Game.snake import Snake
 from Game.gameLogic import GameLogic
 from Graphics.background import Background
 from Game.food import Food
+import Game.colors as color
+
 WIDTH, HEIGHT = 1400, 900
 SCREEN_WIDTH, SCREEN_HEIGHT = 900, 800
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 
-# Màu sắc
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GRAY  = (96, 100, 107)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
 
 def display_message(message, screen, screen_size):
     popup_font = pygame.font.Font(None, 48)
-    popup_text = popup_font.render(message, True, RED)
+    popup_text = popup_font.render(message, True, color.RED)
     popup_rect = popup_text.get_rect(center=(screen_size[0] // 2, screen_size[1] // 2))
     screen.blit(popup_text, popup_rect)
     
@@ -56,34 +52,40 @@ def main():
                         snake.change_direction((1, 0))
 
         background.draw(window)
-
+        
         if not game_logic.game_over():
             game_logic.update()
-
-
+            
         # Vẽ screen
-        screen.fill(BLACK)
+        screen.fill(color.BLACK)
         for x in range(GRID_WIDTH):
             for y in range(GRID_HEIGHT):
-                pygame.draw.rect(screen, GRAY, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE), 1)
+                pygame.draw.rect(screen, color.GRAY, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE), 1)
+                
         # Vẽ khung viền ngoài
-        pygame.draw.rect(screen, WHITE, (0, 0, SCREEN_WIDTH + 1, SCREEN_HEIGHT + 1), GRID_SIZE )
+        pygame.draw.rect(screen, color.WHITE, (0, 0, SCREEN_WIDTH + 1, SCREEN_HEIGHT + 1 ), 3 )
         
-        pygame.draw.rect(screen, RED, (food.food[0] * GRID_SIZE, food.food[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+        # food
+        pygame.draw.rect(screen, color.RED, (food.food[0] * GRID_SIZE, food.food[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+        
         # Vẽ rắn
         for segment in snake.body:
-            pygame.draw.rect(screen, GREEN, (segment[0] * GRID_SIZE, segment[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(screen, color.GREEN, (segment[0] * GRID_SIZE, segment[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
 
         window.blit(screen, (50, 50))
-
+        
+        # score
+        score = game_logic.get_score()
+        
         if game_logic.game_over():
-            screen.fill(BLACK)
-            score = game_logic.get_score()
+            screen.fill(color.BLACK)
+            
             display_message(f"Game Over - Press SPACE to restart\nYour scores: {score}", screen, (SCREEN_WIDTH, SCREEN_HEIGHT))
             window.blit(screen, (50, 50))
 
         pygame.display.update()
         clock.tick(10)
+
     pygame.quit()
 if __name__ == "__main__":
     main()
