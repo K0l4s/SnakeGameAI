0f06490ca996fb1f8a2d13db9cec79763f080175
MinhThuan <thuanngo454@gmail.com>
2023-11-15 23:25:22 +0700

    prepare pull

--- main.py
+++ main.py
@@ -194,7 +194,7 @@ def main():
             window.blit(screen, (30, 30))
         
         if using_algorithm:
-            clock.tick(2500)
+            clock.tick(50)
         else:
             clock.tick(15)
         if using_algorithm and selected_alogrithm == "BFS":
