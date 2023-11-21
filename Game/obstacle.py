396d64060d15d08e90c4b8836aee065abfa6b349
MinhThuan <thuanngo454@gmail.com>
2023-11-21 16:15:50 +0700

    add create map

--- Game/obstacle.py
+++ Game/obstacle.py
@@ -7,6 +7,13 @@ class Obstacle:
         self.size = 20
         self.image = pygame.image.load("Resources/block.jpg")
         self.image = pygame.transform.scale(self.image, (self.size, self.size))
+        self.selected = False
 
     def draw(self, screen):
         screen.blit(self.image, (self.x * 20, self.y * 20))
+
+    def toggle_selection(self):
+        self.selected = not self.selected
+    
+    def is_selected(self):
+        return self.selected
